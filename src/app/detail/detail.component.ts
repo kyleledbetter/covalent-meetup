c1a12f765b2c540c66e45cdd603d7f2b7ff42dde
Kyle Ledbetter <kyle.ledbetter@teradata.com>
2016-10-10 10:46:52 -0500

    update(detail): users serve & back function

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Location } from '@angular/common';
+
 import { Router, ActivatedRoute } from '@angular/router';
 
-import { ItemsService } from '../../services';
+import { ItemsService, UsersService } from '../../services';
 
 @Component({
   selector: 'qs-detail',
   templateUrl: 'detail.component.html',
   styleUrls: ['detail.component.scss'],
-  viewProviders: [ ItemsService ],
+  viewProviders: [ ItemsService, UsersService ],
 })
 export class DetailComponent implements OnInit {
 
   item: Object = {};
+  users: Object[];
 
-  constructor(private _router: Router, private _itemsService: ItemsService, private _route: ActivatedRoute) {}
+  constructor(private _router: Router, private _itemsService: ItemsService,
+              private _usersService: UsersService, private _route: ActivatedRoute,
+              private _location: Location) {}
 
   goBack(): void {
-    this._router.navigate(['/dashboard-product']);
+    this._location.back();
   }
 
   ngOnInit(): void {
+    this._usersService.query().subscribe((users: Object[]) => {
+      this.users = users;
+    });
     this._route.params.subscribe((params: {id: string}) => {
       let itemId: string = params.id;
       this._itemsService.get(itemId).subscribe((item: Object) => {
